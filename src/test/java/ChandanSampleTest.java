8045dbad435146171675b50b75bb1b3ffc2b0556
Chandankumar <chandanraayappab@gmail.com>
2022-06-18 19:09:09 +0530

    chandan commit

---
+++ src/test/java/ChandanSampleTest.java
@@ -0,0 +1,10 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ChandanSampleTest {
+	@Test
+	public void chandanSampleTest() {
+		Reporter.log("ChandanSampleTest",true);
+	}
+
+}
